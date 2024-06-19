7897a986bbf1282010568b3e564513893658dec6
ismatillo marufkhonov <marufkhonovismatillo@gmail.com>
2024-06-19 15:27:18 +0500

    [localization] added localization

--- 7Saber-main/_Saber_mainApp.swift
+++ 7Saber-main/_Saber_mainApp.swift
@@ -19,10 +19,11 @@ struct _Saber_mainApp: App {
         WindowGroup {
             if (DataStorage.storage.get(from: .isRegistrate) as? Bool) == true || skipButtonPressed  {
                 TabBarView()
+                    .environmentObject(registerVM)
             } else {
                 RegisterMobillAppView(skipButtonTapped: {
                     skipButtonPressed = true
-                })
+                }, hasSkipButton: true)
                     .environmentObject(registerVM)
             }
         }
