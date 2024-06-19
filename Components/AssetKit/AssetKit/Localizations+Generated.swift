f6f087857fbb1b9eff01089980adfe8b39d5d605
ismatillo marufkhonov <marufkhonovismatillo@gmail.com>
2024-06-19 16:42:53 +0500

    some change

--- Components/AssetKit/AssetKit/Localizations+Generated.swift
+++ Components/AssetKit/AssetKit/Localizations+Generated.swift
@@ -27,8 +27,7 @@ public enum Localizations {
 
 extension Localizations {
   private static func tr(_ table: String, _ key: String, _ args: CVarArg..., fallback value: String) -> String {
-    let format = BundleToken.bundle.localizedString(forKey: key, value: value, table: table)
-    return String(format: format, locale: Locale.current, arguments: args)
+    BundleToken.bundle.localizedString(forKey: key, value: value, table: table)
   }
 }
 
