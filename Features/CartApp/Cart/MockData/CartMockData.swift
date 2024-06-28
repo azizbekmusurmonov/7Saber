718b33605405e110b41deb617f10dc98e2026357
ismatillo marufkhonov <marufkhonovismatillo@gmail.com>
2024-06-28 10:21:37 +0500

    [cart] added mock data to cart

---
+++ Features/CartApp/Cart/MockData/CartMockData.swift
@@ -0,0 +1,41 @@
+//
+//  CartMockData.swift
+//  Cart
+//
+//  Created by Ismatillokhon on 28/06/24.
+//
+
+import Foundation
+
+final class CartMockData {
+    static let product = ProductModel(
+        id: UUID().hashValue, catalogID: UUID().hashValue, categoryID: UUID().hashValue,
+        sportTypeID: UUID().hashValue, nameUz: "Product", nameRu: "Product", nameEn: "Profuct",
+        descUz: "Description", descRu: "Description", descEn: "Description", specUz: "specUz", specRu: "specRu", 
+        specEn: "specRu", sku: "", discount: [], price: ["price" : 100000], gender: "male", mainImgID: 0,
+        mediaID: "", clientMediaIDS: "", weight: 0, type: 0, isActive: true, additions: [], createdAt: "",
+        updatedAt: "", name: "Ismatillo", desc: "Marufkhonov", attributes: [
+            CartAttribute(productID: UUID().hashValue, billzID: "", barcode: 0, colorID: 0, size: "XS", photos: [""], qty: 0, createdAt: "", updatedAt: "",
+                          color: .init(id: UUID().hashValue, billzID: "", nameUz: "Color", nameRu: "Color", nameEn: "Color", hex: "#FF0000", createdAt: "", updatedAt: "", name: "Red Color")
+                         )
+        ],
+        mainImg: .init(id: 0, src: "https://7saber.uz/storage/product/12067ZXHeCc4CwRurnTZP.jpg", type: ""), media: "")
+    
+    static let data: [Cart] = [
+        Cart(id: UUID().hashValue, userID: UUID().hashValue, productID: UUID().hashValue,
+             qty: 0, details: .init(colorID: UUID().hashValue, size: "XS"), type: 0,
+             createdAt: "", updatedAt: "", product: CartMockData.product),
+        Cart(id: UUID().hashValue, userID: UUID().hashValue, productID: UUID().hashValue,
+             qty: 0, details: .init(colorID: UUID().hashValue, size: "XX"), type: 0,
+             createdAt: "", updatedAt: "", product: CartMockData.product),
+        Cart(id: UUID().hashValue, userID: UUID().hashValue, productID: UUID().hashValue,
+             qty: 0, details: .init(colorID: UUID().hashValue, size: "L"), type: 0,
+             createdAt: "", updatedAt: "", product: CartMockData.product),
+        Cart(id: UUID().hashValue, userID: UUID().hashValue, productID: UUID().hashValue,
+             qty: 0, details: .init(colorID: UUID().hashValue, size: "N"), type: 0,
+             createdAt: "", updatedAt: "", product: CartMockData.product),
+        Cart(id: UUID().hashValue, userID: UUID().hashValue, productID: UUID().hashValue,
+             qty: 0, details: .init(colorID: UUID().hashValue, size: "M"), type: 0,
+             createdAt: "", updatedAt: "", product: CartMockData.product),
+    ]
+}
